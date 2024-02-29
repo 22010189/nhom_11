4224d50958dfb72ce01ac3909a589d844cb4ddd6
Gigasama <160729165+Gigasama@users.noreply.github.com>
2024-02-29 17:05:54 +0700

    Update Book.java

--- src/main/java/com/mycompany/app/Book.java
+++ src/main/java/com/mycompany/app/Book.java
@@ -1,4 +1,11 @@
-class Book{
-
-    
-}
+public class Book {
+ String title;
+ String author;
+ int numPages;
+ Book() { } ; // default constructor
+ public Book(String t, String a, int p) {
+ title = t;
+ author = a;
+ numPages = p;
+ }
+ 
