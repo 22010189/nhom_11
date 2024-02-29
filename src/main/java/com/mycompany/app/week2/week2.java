f8a850f35f0947f39efd100a8ab33d83f434cbfa
Gigasama <160729165+Gigasama@users.noreply.github.com>
2024-02-29 17:54:41 +0700

    Update week2.java

--- src/main/java/com/mycompany/app/week2/week2.java
+++ src/main/java/com/mycompany/app/week2/week2.java
@@ -1,1 +1,13 @@
 
+public class Book {
+ String title;
+ String author;
+ int numPages;
+ Book() { } ; // default constructor
+ public Book(String t, String a, int p) {
+ title = t;
+ author = a;
+ numPages = p;
+ }
+}
+ 
