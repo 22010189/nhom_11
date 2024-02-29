fbf7d163614c0668f7e94bf6f084fbf01a5704c9
Gigasama <160729165+Gigasama@users.noreply.github.com>
2024-02-29 21:03:18 +0700

    Create recursion.java

---
+++ src/main/java/com/mycompany/app/recursion.java
@@ -0,0 +1,11 @@
+public class Recursion {
+    
+    public static int Recursion(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return n * Recursion(n - 1);
+        }
+    }
+}
+    
