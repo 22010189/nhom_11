dc85d9777d8a220003f4c4efbe8612274fc5491c
Quốc Ngọc <121930739+22010189@users.noreply.github.com>
2024-02-29 21:43:19 +0700

    Update InterfacesExercise1.java

--- src/main/java/com/mycompany/ Exercises_on_Book/InterfacesExercise1.java
+++ src/main/java/com/mycompany/ Exercises_on_Book/InterfacesExercise1.java
@@ -1,7 +1,7 @@
 package com.mycompany.Exercises_on_Book
-
+import InterfacesExercise1_1;
 public class InterfacesExercise1{
      public static void main(String[] args){
-       System.out.println("heloo");
+         
      }
 }
