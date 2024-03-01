f0bfde04fb6a06f177baa4758fb60e10a074867e
Quốc Ngọc <121930739+22010189@users.noreply.github.com>
2024-03-01 01:14:47 +0000

    delete

--- src/main/java/com/mycompany/ Exercises_on_Book/Reusing1.java
+++
@@ -1,9 +0,0 @@
-public class FirstClass {
-    FirstClass() {
-        System.out.println("FirstClass object initialized");
-    }
-
-    void firstMethod() {
-        System.out.println("FirstClass method called");
-    }
-}
