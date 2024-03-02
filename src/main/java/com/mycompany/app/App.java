3aee99ae58142bc76bad2213b07df12a945a79d5
Quốc Ngọc <121930739+22010189@users.noreply.github.com>
2024-03-02 09:31:21 +0700

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -4,7 +4,7 @@ import PhuongThuc.java;
 public class App {
     public static void main(String[] args) {
         PhuongThuc a = new PhuongThuc();
-        a.helo();
+        helo();
         System.out.println("Hello Remote World!");
         
     }
