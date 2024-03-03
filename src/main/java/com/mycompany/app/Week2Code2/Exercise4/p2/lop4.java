b90d15960af10060891f20ecc244ed5113eef083
Quốc Ngọc <121930739+22010189@users.noreply.github.com>
2024-03-04 04:18:42 +0700

    Create lop4.java

---
+++ src/main/java/com/mycompany/app/Week2Code2/Exercise4/p2/lop4.java
@@ -0,0 +1,9 @@
+package p2;
+public class lop4 extends lop1 {
+    public void D {
+        // Tạo một đối tượng của lớp D
+        lop4 d = new lop4();
+        // Gọi phương thức A của đối tượng d
+        d.A(); // Có thể truy cập được vì là lớp con của lớp A
+    }
+}
