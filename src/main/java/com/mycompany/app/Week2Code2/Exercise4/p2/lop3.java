c25d471dd0d406c0574dfe25bc01c09d534f9b4f
Quốc Ngọc <121930739+22010189@users.noreply.github.com>
2024-03-04 04:10:05 +0700

    Create lop3.java

---
+++ src/main/java/com/mycompany/app/Week2Code2/Exercise4/p2/lop3.java
@@ -0,0 +1,8 @@
+package p2;
+public class lop3 {
+    public void  C{
+        lop1 a = new lop1();
+        // Gọi phương thức A của đối tượng a
+        a.A(); // Không thể truy cập được vì khác gói package
+    }
+}
