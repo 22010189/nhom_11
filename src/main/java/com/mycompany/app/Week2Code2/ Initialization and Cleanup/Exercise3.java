af8913cf5350d154a7c8dcf7ac5f4595946d8bf6
Gigasama <160729165+Gigasama@users.noreply.github.com>
2024-03-03 21:09:01 +0700

    Create Exercise3.java

---
+++ src/main/java/com/mycompany/app/Week2Code2/ Initialization and Cleanup/Exercise3.java
@@ -0,0 +1,16 @@
+public class MessagePrinter {
+    
+    public MessagePrinter() {
+        System.out.println("Default message");
+    }
+
+    public MessagePrinter(String message) {
+        System.out.println("Custom message: " + message);
+    }
+
+    public static void main(String[] args) {
+        MessagePrinter printer1 = new MessagePrinter();
+
+        MessagePrinter printer2 = new MessagePrinter("Hello there!");
+    }
+}
