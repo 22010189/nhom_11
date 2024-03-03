fdcace5860c8cfb70d5444e1ec5075aeed947dc9
Quốc Ngọc <121930739+22010189@users.noreply.github.com>
2024-03-04 04:08:47 +0700

    Update lop1.java

--- src/main/java/com/mycompany/app/Week2Code2/Exercise4/p1/lop1.java
+++ src/main/java/com/mycompany/app/Week2Code2/Exercise4/p1/lop1.java
@@ -1,8 +1,8 @@
 package p1;
 
 public class lop1 {
-    protected void display() {
-        System.out.println("This is a protected method");
+    protected void A() {
+        System.out.println("Xin chao");
     }
 }
 
