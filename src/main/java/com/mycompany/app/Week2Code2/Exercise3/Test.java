c8a2c2dccca4e6a9662e9833e07f61e1845f6573
Quốc Ngọc <121930739+22010189@users.noreply.github.com>
2024-03-03 08:49:33 +0700

    Create Test.java

---
+++ src/main/java/com/mycompany/app/Week2Code2/Exercise3/Test.java
@@ -0,0 +1,7 @@
+import com.mycompany.app.Week2Code2.Exercise3.debug.Debug;
+public class test {
+      public static  void main(String[] args) {
+		Debug d = new Degbug();
+		d.debug("xinchao");
+	}
+}
