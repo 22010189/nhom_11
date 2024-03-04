a4217b97da373397e5852e3d98b127f496cb992f
Quốc Ngọc <121930739+22010189@users.noreply.github.com>
2024-03-04 09:42:00 +0700

    Update Exercise6.java

--- src/main/java/com/mycompany/app/Week2Code2/Exercise6.java
+++ src/main/java/com/mycompany/app/Week2Code2/Exercise6.java
@@ -1,15 +1,18 @@
 package com.mycompany.app.Week2Code2;
 
 public class Exercise6 {
-    class LopDcBaoVe {
+    public class LopDcBaoVe {
          protected int data;
 
-         public LopDcBaoVe(int data) {
+         public void LopDcBaoVe(int data) {
              this.data = data;
             }
+         public int getData(){
+            return data;
+         }
         }
 
-    class LopThayDoiLopBaoVe {
+    public class LopThayDoiLopBaoVe {
             public void LopThayDoiLopBaoVe(LopDcBaoVe a, int value) {
             a.data += value;
             }
