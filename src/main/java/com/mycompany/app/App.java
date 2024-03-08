7eb608aec31f4c09b072bd267ddb737ce55ba2a2
Gigasama <160729165+Gigasama@users.noreply.github.com>
2024-03-08 08:44:27 +0700

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,52 +1,15 @@
+
 package com.mycompany.app;
-import com.mycompany.app.Week2Code3.PhuongThuc;
-import com.mycompany.app.Week1Code2.Each_loop;
-import com.mycompany.app.Week1Code2.SwitCH;
-import com.mycompany.app.Week2Code2.AccessControl.Exercise5.vidu;
-import com.mycompany.app.Week2Code2.AccessControl.Exercise6;
-import com.mycompany.app.Week2Code2.AccessControl.Exercise3.debug.Debug;
-import com.mycompany.app.Week2Code2.ReusingClassess.*;
-public class App { 
+import com.mycompany.app.week2.B;
+import com.mycompany.app.week3.*;
+import com.mycompany.app.week3.PolymorphismExercise17.Bicycles;
+import com.mycompany.app.week3.PolymorphismExercise17.Cycles;
+import com.mycompany.app.week3.PolymorphismExercise17.Tricycles;
+import com.mycompany.app.week3.PolymorphismExercise17.Unicycles;
+public class App {
     public static void main(String[] args) {
-        Debug d = new Debug();
-        d.debug("xinchao");
-
-        System.out.println("Hello Remote World!");
-        
-        PhuongThuc a = new PhuongThuc();
-        a.helo();
-        
-        Each_loop b = new Each_loop();
-        b.E();
-
-        SwitCH c = new SwitCH();
-        c.d();
-
-        vidu t = new vidu();
-        
-        System.out.println(t.a); // Có thể truy cập được vì là public
-   //   System.out.println(t.b); // Không thể truy cập được vì là private
-   //   System.out.println(t.c); // Không thể truy cập được vì là protected
-   //   System.out.println(t.d); // Không thể truy cập được vì là package-access
-
-        t.m1(); // Có thể truy cập được vì là public
-     // t.m2(); // Không thể truy cập được vì là private
-     // t.m3(); // Không thể truy cập được vì là protected
-     // t.m4(); // Không thể truy cập được vì là package-access
-        Exercise6 g = new Exercise6();
-        Exercise6.LopDcBaoVe gg =g.new LopDcBaoVe();
-        gg.LopDcBaoVe(5);
-        System.out.println(gg.getData());
-        
-        EXercise1 e = new EXercise1();
-        EXercise1.SecondClass ee = e.new SecondClass();
-        EXercise1.FirstClass eee = ee.getObj(100);
-        System.out.println(eee.getValue());
-
-
-
-        
-        
+        Cycles[] c = {new Unicycles(), new Bicycles(),new Tricycles()};
+            ((Unicycles)c[0]).balance();
+		    ((Bicycles)c[1]).balance();
     }
 }
-
